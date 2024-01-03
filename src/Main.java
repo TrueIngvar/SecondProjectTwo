35dac1697172b08faa55a84f3f25d49255ce5cf4
TrueIngvar <igor171298@mail.ru>
2024-01-03 16:53:46 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int fsum = 100;
+        int addSum = 1100;
+        int toPlusToS = 100;
+        int eq = addSum/toPlusToS;
+        if (addSum >= 1000) {
+            System.out.println(fsum + addSum + eq);
+
+            //System.out.println(fsum + addSum + plusToS);
+        } else {
+            System.out.println(fsum + addSum);
+        }
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+    }
+}
