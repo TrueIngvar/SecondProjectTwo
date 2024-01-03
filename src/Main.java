d7d06b88b5ff64e279b820e00c439bd620aafaa2
TrueIngvar <igor171298@mail.ru>
2024-01-03 16:56:50 +0400

    Changed

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,8 @@ public class Main {
         int eq = addSum/toPlusToS;
         if (addSum >= 1000) {
             System.out.println(fsum + addSum + eq);
-
-            //System.out.println(fsum + addSum + plusToS);
         } else {
             System.out.println(fsum + addSum);
         }
-        // Объявляете переменные для входных данных и
-        // параметров программы: начального счёта,
-        // суммы пополнения и тп
-
-        // Условным оператором проверяете, превысила ли
-        // сумма пополнения порог, и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-        // на экран.
     }
 }
