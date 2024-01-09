e9d65a5d5d6585645ce26cee55449665d6823c92
TrueIngvar <igor171298@mail.ru>
2024-01-09 14:55:20 +0400

    Changed condition

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int firstSum = 100;
-        int addSum = 1000;
+        int addSum = 1100;
         int divideAddSum = 100;
         int addToAllSum = addSum / divideAddSum;
-        if (addSum >= 1000) {
+        if (addSum > 1000) {
             System.out.println(firstSum + addSum + addToAllSum);
         } else {
             System.out.println(firstSum + addSum);
